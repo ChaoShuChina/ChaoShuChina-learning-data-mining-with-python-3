6374f658ecb0cabf1ff89cd7141bbc3cd74dbab2
ChaoShuChina <824372863@qq.com>
2017-05-09 17:17:18 +0800

    change 3

--- demo1.py
+++ demo1.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from collections import defaultdict
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import cross_val_score
+import numpy as np
 
 dataset = pd.read_csv('2013-2014_Games.csv', parse_dates=['Date'])
 dataset.columns = ['Date', 'StartTime', 'VisitorTeam', 'VisitorPts', 'HomeTeam', 'HomePts', 'OT?']
@@ -19,4 +22,10 @@ for index, row in dataset.sort('Date').iterrows():
     won_last[HomeTeam] = row['HomeWin']
     won_last[VisitorTeam] = not row['HomeWin']
 
-print(dataset.ix[20:25])
+# print(dataset.ix[20:25])
+clf = DecisionTreeClassifier(random_state=14)
+x_previouswins = dataset[['HomeLastWin', 'VisitorLastWin']].values
+
+scores = cross_val_score(clf, x_previouswins, y_true)
+out = np.mean(scores) * 100
+print out
