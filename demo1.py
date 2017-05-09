63a39cd91244715d4521a25974bcf13b2f2a5867
ChaoShuChina <824372863@qq.com>
2017-05-09 16:51:40 +0800

    change 2

--- demo1.py
+++ demo1.py
@@ -4,6 +4,9 @@ from collections import defaultdict
 dataset = pd.read_csv('2013-2014_Games.csv', parse_dates=['Date'])
 dataset.columns = ['Date', 'StartTime', 'VisitorTeam', 'VisitorPts', 'HomeTeam', 'HomePts', 'OT?']
 dataset['HomeWin'] = dataset['VisitorPts'] < dataset['HomePts']
+dataset['HomeLastWin'] = False
+dataset['VisitorLastWin'] = False
+
 y_true = dataset['HomeWin'].values
 
 won_last = defaultdict(int)
@@ -16,4 +19,4 @@ for index, row in dataset.sort('Date').iterrows():
     won_last[HomeTeam] = row['HomeWin']
     won_last[VisitorTeam] = not row['HomeWin']
 
-
+print(dataset.ix[20:25])
