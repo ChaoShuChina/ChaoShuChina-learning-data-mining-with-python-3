af02523a28875849de9a1592dc02cc9bb79c8ba8
ChaoShuChina <824372863@qq.com>
2017-05-09 14:54:20 +0800

    change 1

--- demo1.py
+++ demo1.py
@@ -1,7 +1,15 @@
 import pandas as pd
 from collections import defaultdict
+
 dataset = pd.read_csv('2013-2014_Games.csv', parse_dates=['Date'])
 dataset.columns = ['Date', 'StartTime', 'VisitorTeam', 'VisitorPts', 'HomeTeam', 'HomePts', 'OT?']
 print(dataset.ix[:])
 dataset['HomeWin'] = dataset['VisitorPts'] < dataset['HomePts']
 y_true = dataset['HomeWin'].values
+
+won_last = defaultdict(int)
+for index, row in dataset.iterrows():
+    HomeTeam = row['HomeTeam']
+    VisitorTeam = row['VisitorTeam']
+    row['HomeLastWin'] = won_last[HomeTeam]
+    row['']
